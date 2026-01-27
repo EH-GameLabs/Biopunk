ab6b144861dd7c16e791c069a5c1620dea6f48ad
Francesco Adami <francescoadami9@gmail.com>
2026-01-27 10:00:07 +0100

    Organizzato progetto (Cartelle e Managers)
    - Cartelle come su TDD
    - Managers in Source > BioPunk > BIOPUNK > SubSystems

---
+++ Source/BioPunk/BIOPUNK/SubSystems/ObjectPoolerWorldSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "ObjectPoolerWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIOPUNK_API UObjectPoolerWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BioPunk/BIOPUNK/SubSystems/SoundManagerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SoundManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIOPUNK_API USoundManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
