28d2c184afbf13218425f5b225575bab916def53
Francesco Adami <francescoadami9@gmail.com>
2026-02-26 16:45:55 +0100

    Health component - Enemy Attack - Perfect Dodge

---
+++ Source/BioPunk/BIOPUNK/Components/HealthComponent.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BIOPUNK/Components/HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	OnDie.AddDynamic(this, &UHealthComponent::NativeDie);
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UHealthComponent::NativeTakeDamage(AActor* Interactor, float Damage)
+{
+	IDamageable::NativeTakeDamage(Interactor, Damage);
+	
+	SetHealth(Health - Damage);
+	
+	// lancio evento di TakeDamage
+	OnTakeDamage.Broadcast(Damage);
+}
+
+void UHealthComponent::NativeDie()
+{
+	IDamageable::NativeDie();
+	
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		2.5f,
+		FColor::Black,
+		FString::Printf(TEXT("MORTO"))
+	);
+}
+
+void UHealthComponent::SetHealth(float const NewHealth)
+{
+	if (Health == NewHealth) return;
+	
+	Health = NewHealth;
+	
+	// lancio evento di cambio vita
+	OnHealthChange.Broadcast(NewHealth);
+	
+	// controllo se è morto
+	if (Health <= 0.0f)
+	{
+		OnDie.Broadcast();
+	}
+}
+
---
+++ Source/BioPunk/BIOPUNK/Interfaces/Damageable.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Damageable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UDamageable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class BIOPUNK_API IDamageable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	// ===== TAKE DAMAGE =====
+	// TakeDamage general version to call in c++
+	void TakeDamage(AActor* Interactor, float Damage) { NativeTakeDamage(Interactor, Damage); }
+
+	// TakeDamage to implement in c++
+	virtual void NativeTakeDamage(AActor* Interactor, float Damage)
+	{
+		UObject* Object = Cast<UObject>(this);
+		Execute_BP_TakeDamage(Object, Interactor, Damage);
+	}
+
+	// TakeDamage to implement in blueprint
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Damageable")
+	void BP_TakeDamage(AActor* Interactor, float Damage);
+	
+	
+	// ===== DIE =====
+	// Die general version to call in c++
+	void Die() { NativeDie(); }
+
+	// Die to implement in c++
+	virtual void NativeDie()
+	{
+		BP_Die();
+	}
+
+	// Die to implement in blueprint
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Damageable")
+	void BP_Die();
+};
